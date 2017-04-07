7a82c6df3fbc421cd733b6e7626b4ac079bdda62
badunoff <badunoff@yahoo.com>
2017-04-07 10:03:04 -0400

    photo - serialized but untested

--- src/application/Photo.java
+++ src/application/Photo.java
@@ -9,8 +9,13 @@ public class Photo implements Serializable{
 	
 	private String description;
 	private Date date;
+	private Date lastModified;
 	private String photoAddress;
 	private List<String> people;
+	private List<String> events;
+	private List<String> locations;
+	
+	public static final String photoDir = "Photos"; 
 	
 	public static final String storeDir = "savedObjects";
 	public static final String storeFile = "Photos.dat"; 
@@ -20,10 +25,53 @@ public class Photo implements Serializable{
 		this.photoAddress = photoAddress;
 	}
 	
+	public String getDescription(){
+		return description;
+	}
 	
+	public Date getDate(){
+		return date;
+	}
 	
+	public Date getLastModified(){
+		return lastModified;
+	}
 	
+	public String getPhoto(){
+		return photoAddress;
+	}
 	
+	public List<String> getPeople(){
+		return people;
+	}
+	
+	public List<String> getEvents(){
+		return events;
+	}
+	
+	public List<String> getLocations(){
+		return locations;
+	}
+	
+	public void setDescription(String description){
+		this.description = description;
+	}
+	
+	public void setPeople(List<String> people){
+		this.people = people;
+	}
+	
+	public void setEvents(List<String> events){
+		this.events = events;
+	}
+	
+	public void setLocations(List<String> locations){
+		this.locations = locations;
+	}
+	
+	public void setDate(Date date){
+		this.date = date;
+	}
 	
 	public static void writeApp(Photo photo) throws IOException {
 		ObjectOutputStream oos = new ObjectOutputStream(
