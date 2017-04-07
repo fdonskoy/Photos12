068d6b9f4d285cb9cd7070d8eb9575e35d6d6909
badunoff <badunoff@yahoo.com>
2017-04-07 09:24:05 -0400

    initial

---
+++ Photos12/src/application/Photos.java
@@ -0,0 +1,11 @@
+package application;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Photos implements Serializable{
+	private String description;
+	private Date date;
+	private String photoAddress;
+	
+}
