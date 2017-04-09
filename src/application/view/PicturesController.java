66022d8c664f810bb38fee994057873eef1f233c
Fil Donskoy <fildonskoy@gmail.com>
2017-04-09 16:22:27 -0400

    changed field member to album

--- src/application/view/PicturesController.java
+++ src/application/view/PicturesController.java
@@ -5,19 +5,22 @@ import java.util.ArrayList;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import application.Album;
 import application.Photo;
 
 public class PicturesController {
-	public static ArrayList<Photo> photos;
+	public static Album album;
 	public static int selectedPhotoIndex;
 	
 	@FXML Button add;
 	
 	public void startSlideShow() throws IOException{
-		if(photos.size() > 0){
+		if(album.getPhotos().size() > 0){
 			LoginController.currentUser.writeUser();
 			SceneLoader.getInstance().changeScene("SlideShow.fxml");
 		}
 	}
+	
+	
 	
 }
